ac692fd96b0433df97cee8718fc6e98227987a81
Nitesh kumar <niteshkumaryadav813@gmail.com>
2024-10-12 11:18:30 +0530

    update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,3 +1,78 @@
+
+
+
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/../Assets/Nitesh_Resume 7.2.pdf";
+import resumeImage from "../../Assets/Nitesh_Resume 7.2.jpg"; 
+import { AiOutlineDownload } from "react-icons/ai";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        <Row className="resume">
+          {/* Display the resume image instead of the PDF */}
+          <img
+            src={resumeImage}
+            alt="Resume"
+            style={{ width: width > 786 ? "55%" : "100%" }}
+            className="d-flex justify-content-center"
+          />
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import React, { useState, useEffect } from "react";
 // import { Container, Row } from "react-bootstrap";
 // import Button from "react-bootstrap/Button";
@@ -60,64 +135,3 @@
 // }
 
 // export default ResumeNew;
-
-
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Nitesh_Resume 7.1.pdf";
-import resumeImage from "../../Assets/Nitesh_Resume 7.1.jpg"; // Import the image
-import { AiOutlineDownload } from "react-icons/ai";
-
-function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
-  return (
-    <div>
-      <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
-        <Row className="resume">
-          {/* Display the resume image instead of the PDF */}
-          <img
-            src={resumeImage}
-            alt="Resume"
-            style={{ width: width > 786 ? "55%" : "100%" }}
-            className="d-flex justify-content-center"
-          />
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-      </Container>
-    </div>
-  );
-}
-
-export default ResumeNew;
-
